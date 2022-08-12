283d1adedec97c4d036430fd2402d813455be7d4
Lenny Ramroop <32972277+BrownieQQ@users.noreply.github.com>
2022-08-12 10:16:31 -0400

    Create app.js

---
+++ Secret Message/app.js
@@ -0,0 +1,16 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 
+'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+
+secretMessage[7] = 'right';
+
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+secretMessage.splice(6, 5, 'know,')
+
+console.log(secretMessage.join(' '));
